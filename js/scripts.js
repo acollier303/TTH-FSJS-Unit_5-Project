eb7aac647323fc07763aed9b25936949d0c7e8cb
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-10-28 09:18:39 -0600

    update modal creation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const gallery = document.getElementById('gallery');
 const search = document.querySelector('search-container');
-const modal = document.getElementsByClassName('modal');
+const modal = document.getElementsByClassName('modal-container');
 const card = document.querySelectorAll('card');
 
 // ------------------------------------------
@@ -34,32 +34,49 @@ function generateProfile(data){
                 <p class="card-text">${person.email}</p>
                 <p class="card-text cap">${person.location.city}, ${person.location.state}s</p>
             </div>
-        </div>`).join('');
+        </div>
+
+        <div class="modal-container">
+            <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                    <p class="modal-text">${person.email}</p>
+                    <p class="modal-text cap">${person.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${person.phone}</p>
+                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${person.phone}</p>
+                </div>
+        </div>
+    
+        `).join('');
     gallery.innerHTML = profileHtml;
     
     //---------- Generate Modal -----------
-    const modalContainer = document.getElementsByClassName('modal-container');
-    const modalHtml = data.map(person => `
-    <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-            <p class="modal-text">${person.email}</p>
-            <p class="modal-text cap">${person.location.city}</p>
-            <hr>
-            <p class="modal-text">${person.phone}</p>
-            <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
-            <p class="modal-text">Birthday: ${person.phone}</p>
-        </div>
-    </div>`).join('');
-    modal.innerHTML = modalHtml;
+    // const modalContainer = document.getElementsByClassName('modal-container');
+    // const modalHtml = data.map(person => `
+    // <div class="modal-container">
+    // <div class="modal">
+    //     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    //     <div class="modal-info-container">
+    //         <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
+    //         <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+    //         <p class="modal-text">${person.email}</p>
+    //         <p class="modal-text cap">${person.location.city}</p>
+    //         <hr>
+    //         <p class="modal-text">${person.phone}</p>
+    //         <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
+    //         <p class="modal-text">Birthday: ${person.phone}</p>
+    //     </div>
+    // </div>`).join('');
+    // modal.innerHTML = modalHtml;
 
     //------------ Event Listener -----------------
     gallery.addEventListener('click', (e) => {
             console.log(e.target);
-            //modalContainer.style.display = 'block';
+            modal.style.display = 'block';
     });
 }
 
