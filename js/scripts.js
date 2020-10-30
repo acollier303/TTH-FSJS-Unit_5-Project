63ceb10952623df1caae62d7e6a93de0f3f92681
Adrian Collier <30817514+acollier303@users.noreply.github.com>
2020-10-30 09:07:08 -0600

    modal update

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
+/* Treehouse FSJS Techdegree
+ * Project 5 - Public API Requests
+ * scripts.js */
+
 const gallery = document.getElementById('gallery');
 const search = document.querySelector('search-container');
 const modal = document.getElementsByClassName('modal-container');
 const card = document.querySelectorAll('card');
+const modals = document.createElement('DIV');
+
 
 // ------------------------------------------
-//  FETCH FUNCTIONS
+//  FETCH FUNCTION
 // ------------------------------------------
 
+// Event listener for Document loaded
 document.addEventListener('DOMContentLoaded', () => {
     function fetchData (url) {
         return fetch(url)
             .then(res => res.json())
     }
-
+    // Fetch users from api
     fetchData('https://randomuser.me/api/?results=12')
     .then(data => generateProfile(data.results))
+    .then(cardListener())
     .catch(err => console.log(err));
 });
 
@@ -22,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-// Generate Profile DOM elements from fetch data
+// ---- Employee Profile Card ----
 function generateProfile(data){
     const profileHtml = data.map(person =>  `
         <div class="card">
@@ -35,65 +43,47 @@ function generateProfile(data){
                 <p class="card-text cap">${person.location.city}, ${person.location.state}s</p>
             </div>
         </div>`).join('');
-    gallery.innerHTML = profileHtml;
-    
-    //---------- Generate Modal -----------
-    // const modalContainer = document.getElementsByClassName('modal-container');
-    // const modalHtml = data.map(person => `
-    // <div class="modal-container">
-    // <div class="modal">
-    //     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    //     <div class="modal-info-container">
-    //         <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
-    //         <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-    //         <p class="modal-text">${person.email}</p>
-    //         <p class="modal-text cap">${person.location.city}</p>
-    //         <hr>
-    //         <p class="modal-text">${person.phone}</p>
-    //         <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
-    //         <p class="modal-text">Birthday: ${person.phone}</p>
-    //     </div>
-    // </div>`).join('');
-    // modal.innerHTML = modalHtml;
 
-    //------------ Event Listener -----------------
-    gallery.addEventListener('click', (e) => {
-            console.log(e.target);
-    });
+    gallery.innerHTML = profileHtml;
+    generateModal(data);
+    gallery.appendChild(modals);
+    //console.log(gallery.innerHTML);
 }
 
-// -------Generate ProfileOverlay  DOM element --------
 
-// function generateModal(data){
-//     const modalContainer = document.getElementsByClassName('modal-container');
-//     const modalHtml = data.map(person =>`
-//     <div class="modal-container">
-//     <div class="modal">
-//         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//         <div class="modal-info-container">
-//             <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
-//             <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-//             <p class="modal-text">${person.email}</p>
-//             <p class="modal-text cap">${person.location.city}</p>
-//             <hr>
-//             <p class="modal-text">${person.phone}</p>
-//             <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
-//             <p class="modal-text">Birthday: 10/21/2015</p>
-//         </div>
-//     </div>`).join('');
-//     modal.innerHTML = modalHtml;
-// }
+// ------- Employee Modal --------
+function generateModal(data){
+
+    const modalHtml = data.map(person =>`
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${person.picture.thumbnail}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                    <p class="modal-text">${person.email}</p>
+                    <p class="modal-text cap">${person.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${person.phone}</p>
+                    <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}  ${person.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${person.number}</p>
+                </div>
+            </div>
+        </div>`).join('');
+    modals.innerHTML = modalHtml;
+
+    //console.log(modalHtml)
+}
+
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-// function cardListener(){
-//     gallery.addEventListener('click', (e) => {
-//         if (e.target.className === 'card'){
-//             console.log(`${person.name.last}`);
-//         };
-//     });
-// };
+function cardListener(){
+    gallery.addEventListener('click', (e) => {
+        console.log(e.target);
+    });
+};
 
 // ------------------------------------------
 //  POST DATA
